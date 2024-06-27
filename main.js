685035b68a33f4033d534adde44a0e123943b0e5
kvantitpd <kvantitpd@gmail.com>
2024-06-27 15:24:09 +0500

    init

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const light = 100;
 const signal = 0;
+
+async function start() {
+    const data = await fetch('http://192.168.10.86:8080/info').then(res => res.json()).then(console.log)
+}
+
+start()
